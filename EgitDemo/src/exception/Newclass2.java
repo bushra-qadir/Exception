8293af623faf140663c85cc83ea58280918e20b3
Abdul Hakeem <hakeemabdul14@gmail.com>
2016-05-23 16:58:13 +0500

    abdul hakeem

--- EgitDemo/src/exception/Newclass2.java
+++ EgitDemo/src/exception/Newclass2.java
@@ -15,6 +15,7 @@ public class Newclass2 {
 		BufferedReader rd = null;
 		File file=new File(filename);
 		System.out.println("Abdul hakeem (sp13-bse-110)");
+		System.out.println("Abdul hakeem (sp13-bse-110)");
 		//file.createNewFile();
 		try {
 			// Open the file for reading.
