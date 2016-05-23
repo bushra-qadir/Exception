d46772cd4f949eff8296677bf8ec4fa1007319b9
shahbaz <shahbaz.akbar869@gmail.com>
2016-05-23 16:18:47 +0500

    fdjkl

--- EgitDemo/src/exception/Newclass2.java
+++ EgitDemo/src/exception/Newclass2.java
@@ -67,6 +67,8 @@ public class Newclass2 {
 >>>>>>> branch 'master' of https://github.com/bushra-qadir/exception
 
 	}
+System.out.println("please enter your commits below");
+System.out.println("shabaz, roll number:SP13-BSE-130");
 
 
 }
